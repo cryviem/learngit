0b00984f40aac0f27c708daa49b16b694693f18b
Hong An <an23593@gmail.com>
2024-07-14 11:58:34 +0700

    add jmath - a example of library

---
+++ jmath/jmath.c
@@ -0,0 +1,34 @@
+include "stdio.h"
+#include "jmath.h"
+
+#define MAX_INT  (int) 0x7FFFFFFF
+
+void jmath_info()
+{
+    printf("JMATH LIBRARY, just a example of library\n");
+}
+
+int jmath_add(int a, int b)
+{
+    return a + b;
+}
+
+int jmath_sub(int a, int b)
+{
+    return a - b;
+}
+
+int jmath_mul(int a, int b)
+{
+    return a * b;
+}
+
+int jmath_div(int a, int b)
+{
+    if (b == 0)
+    {
+        printf("jmath > error: div to 0\n")
+        return MAX_INT;
+    } 
+    return a / b;
+}
---
+++ jmath/jmath.h
@@ -0,0 +1,10 @@
+#ifndef JMATH_H
+#define JMATH_H
+
+void jmath_info();
+int jmath_add(int a, int b);
+int jmath_sub(int a, int b);
+int jmath_mul(int a, int b);
+int jmath_div(int a, int b);
+
+#endif
